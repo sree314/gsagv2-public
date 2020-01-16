971863502cc135d59ca9e66775b679df602684a1
sree <sree@buckyball>
2020-01-16 15:32:30 -0500

    Add new_assignment.sh script to create initial assignment repository

---
+++ new_assignment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+P=`dirname $0`
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 /path/to/newassignment"
+	echo "newassignment should be the directory inside submitted zip file, e.g. 'a1'"
+	exit 1;
+fi;
+
+DST="$1"
+ASSIGNMENT=`basename "$DST"`
+
+if mkdir -p "$DST"; then
+	cp -va $P/assignment_template/* "$DST" && mv "$DST/gitignore" "$DST/.gitignore"
+	cp -va $P/checkerutils/checkerutils "$DST"
+
+	sed -i "s/ZIP_ROOT = 'aX'.*/ZIP_ROOT = '$ASSIGNMENT'/" "$DST/checker.py"
+
+	if [ ! -f "$DST/ssh/deploy_key" ]; then
+		mkdir -p "$DST/ssh" &&  ssh-keygen -N "" -f "$DST/ssh/deploy_key"
+	fi;
+fi;
