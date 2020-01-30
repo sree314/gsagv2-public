d5531ac76d0023549e6508823dc3eaad7e625e91
sree <sree@buckyball>
2020-01-29 21:25:02 -0500

    Add python3 and echo build stamp

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
 apt-get install -y python python-pip python-dev
 
-apt-get install -y python3-yaml
+apt-get install -y python3-yaml python3-pip python3-dev
 
 apt-get install -y cbmc z3
 
